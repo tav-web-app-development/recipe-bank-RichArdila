612b0f83e04a4bb0babf041b68c537add7fbb4c4
Richard Ardila <richardardilaz17@hotmail.com>
2024-04-06 17:10:27 -0400

    Added html and file components, cofigured Navbar and Footer

---
+++ src/components/Recipes.jsx
@@ -0,0 +1,11 @@
+export default function Recipes () {
+    return (
+        <div className="recipe-container">
+            <div className="recipe">
+                <p><strong>Description:</strong></p>
+                <p><strong>Ingredients:</strong></p>
+                <p><strong>Directions:</strong></p>
+            </div>
+        </div>
+    );
+}
